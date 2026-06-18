c71fb69da23828a147e9297bbde813dbf42e5830
Bogdan <bogdan.levin07@yandex.ru>
2026-06-18 21:59:59 +0300

    Add files via upload

---
+++ mdk01-04-system-programming/1/PipeServer/PipeServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+public class Server
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            using (var server = new NamedPipeServerStream("mypipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("Сервер ожидает подключения...");
+                server.WaitForConnection();
+                Console.WriteLine("Клиент подключен.");
+
+                using (var reader = new StreamReader(server))
+                using (var writer = new StreamWriter(server) { AutoFlush = true })
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line == "/time")
+                        {
+                            writer.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                        }
+                        else if (line.StartsWith("/echo"))
+                        {
+                            string text = line.Length > 5 ? line.Substring(line.StartsWith("/echo ") ? 6 : 5) : "";
+                            writer.WriteLine(string.IsNullOrEmpty(text) ? "(пусто)" : text);
+                        }
+                        else if (line == "/exit")
+                        {
+                            writer.WriteLine("До свидания!");
+                            break;
+                        }
+                        else
+                        {
+                            writer.WriteLine("Неизвестная команда.");
+                        }
+                    }
+                }
+                Console.WriteLine("Клиент отключен.");
+            }
+        }
+    }
+}
---
+++ mdk01-04-system-programming/2/PipeClient/PipeClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+public class Client
+{
+    public static void Main()
+    {
+        using (var client = new NamedPipeClientStream(".", "mypipe", PipeDirection.InOut))
+        {
+            Console.WriteLine("Подключение к серверу...");
+            client.Connect();
+            Console.WriteLine("Подключено.");
+
+            using (var reader = new StreamReader(client))
+            using (var writer = new StreamWriter(client) { AutoFlush = true })
+            {
+                while (true)
+                {
+                    Console.Write("Введите команду: ");
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input)) continue;
+
+                    writer.WriteLine(input);
+
+                    string response = reader.ReadLine();
+                    Console.WriteLine("Ответ: " + response);
+
+                    if (input == "/exit")
+                        break;
+                }
+            }
+        }
+    }
+}
